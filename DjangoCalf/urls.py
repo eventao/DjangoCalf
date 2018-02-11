bd3400329c1556ebd194f18142e6278f4e154a60
eventao <330293577@qq.com>
2018-02-12 00:20:03 +0800

    添加数据库与模型！

--- DjangoCalf/urls.py
+++ DjangoCalf/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from . import view
+from . import view, testdb
 
 urlpatterns = [
     url(r'^$', view.home),
     url(r'^about$', view.about),
     url(r'^contact$', view.contact),
+    url(r'^testdb', testdb.testdb),
 ]
